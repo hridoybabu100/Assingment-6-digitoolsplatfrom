22d337987a45ae9967d25dc6af8d428822c915d0
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-03-30 19:32:43 +0800

    Banner section updated

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { CiShoppingCart } from "react-icons/ci";
+
+const Navbar = () => {
+  return (
+    <div className="py-10 shadow-b flex justify-between items-center max-w-[80%] mx-auto">
+      <div className="">
+        <a className="btn btn-ghost text-xl text-[#4F39F6]">Digitools</a>
+      </div>
+
+      <div className=" flex items-center gap-5">
+        <ul>
+          <li>
+            <a href="#"></a>Products
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#"></a>Pricing
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#"></a>Features
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#"></a>Testimonials
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center gap-5">
+        <p>FAQ</p>
+        <div>
+           <CiShoppingCart/> 
+        </div>
+        <div>
+          <p>Login</p>
+        </div>
+        <div>
+          <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-[#FFFFFF] font-bold">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
